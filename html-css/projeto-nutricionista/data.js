eff584dc3568bb137b948f6b1b895b92c43b9d64
Alberto <betim009@gmail.com>
2024-07-02 21:01:43 -0300

    dados

---
+++ html-css/projeto-nutricionista/data.js
@@ -0,0 +1,18 @@
+export const thead = {
+    thead1: "Calorias (valor energético)",
+    thead2: 1431.00,
+    thead3: 71.55
+}
+
+export const tbody = [
+    {
+        tbody1: "Carboidratos líquidos",
+        tbody2: 0.00,
+        tbody3: "-"
+    },
+    {
+        tbody1: "Carboidratos",	
+        tbody2: 0.00,
+        tdobdy3:0.00
+    }
+];
